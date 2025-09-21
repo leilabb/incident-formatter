110f9537dd7e40ae825de90c257450478093d45f
Leila <leila.benhalima2@gmail.com>
2025-09-21 12:11:53 +0200

    hide tarea

--- src/Form.jsx
+++ src/Form.jsx
@@ -253,7 +253,7 @@ export default function Form({ formData, handleChange }) {
         </div>
       </div>
       {/* <!-- Tarea --> */}
-      <div className="">
+      {/* <div className="">
         <label
           for="tarea"
           className="text-left block text-sm font-medium text-gray-700 mb-2"
@@ -270,7 +270,7 @@ export default function Form({ formData, handleChange }) {
           }}
           className="px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-500 resize-none"
         ></textarea>
-      </div>
+      </div> */}
     </div>
   );
 }
