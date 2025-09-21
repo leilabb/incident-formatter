1fc00f1ab51a3c29fcefc79d963e0b318fe36c33
Leila <leila.benhalima2@gmail.com>
2025-09-21 09:06:27 +0200

    design

--- incident-formatter/src/Form.jsx
+++ incident-formatter/src/Form.jsx
@@ -1,146 +1,124 @@
-import { useState } from "react";
-
 export default function Form({ formData, handleChange }) {
+  const zones = [
+    "SAN PEDRO",
+    "LA MANGA",
+    "MALAGA",
+    "ALGECIRAS",
+    "FUENGIROLA",
+    "HUELVA",
+  ];
+  const tipos = ["D2", "C3"];
+
   return (
     <div className="">
-      {/*  Radio buttons F1, D2, C3  */}
-      <div className="mb-6">
-        <label className="text-left block text-sm font-medium text-gray-700 mb-3">
-          Tipo
-        </label>
+      {/*  Radio buttons D2, C3  */}
+      <div className="flex mb-6 space-x-16">
+        <div className="flex flex-col">
+          <label className="text-left block text-sm font-medium text-gray-700 mb-2">
+            Tipo
+          </label>
+          <div
+            onChange={(event) => {
+              handleChange(event);
+            }}
+            className="flex px-3 py-2 border border-gray-300 rounded-md shadow-sm"
+          >
+            <label className="space-x-2 flex flex-row items-center">
+              {tipos.map((tipo) => {
+                return (
+                  <div>
+                    <span className=" text-gray-700">{tipo}</span>
+                    <input
+                      type="radio"
+                      name="type"
+                      value={tipo}
+                      className="ml-2 text-blue-600 focus:ring-blue-500"
+                      id={tipo}
+                    />
+                  </div>
+                );
+              })}
+            </label>
+          </div>
+        </div>
+
+        {/* <!-- Clientes afectados --> */}
         <div
           onChange={(event) => {
             handleChange(event);
           }}
         >
-          <label className="flex items-center">
-            <input
-              type="radio"
-              name="type"
-              value="F1"
-              className="text-blue-600 focus:ring-blue-500"
-              id="f1"
-            />
-            <span className="ml-2 text-gray-700">F1</span>
+          <label
+            for="clientes"
+            className="text-left block text-sm font-medium text-gray-700 mb-2"
+          >
+            Clientes afectados
           </label>
-          <label className="flex items-center">
-            <input
-              type="radio"
-              name="type"
-              value="D2"
-              className="text-blue-600 focus:ring-blue-500"
-              id="d2"
-            />
-            <span className="ml-2 text-gray-700">D2</span>
-          </label>
-          <label className="flex items-center">
-            <input
-              type="radio"
-              name="type"
-              value="C3"
-              className="text-blue-600 focus:ring-blue-500"
-              id="c3"
-            />
-            <span className="ml-2 text-gray-700">C3</span>
-          </label>
+          <input
+            name="affected"
+            type="number"
+            id="affected"
+            min="0"
+            placeholder="N de clientes"
+            className="w-42 px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-500"
+          />
         </div>
       </div>
 
-      {/*  Zona dropdown --> */}
-      <div
-        className="mb-6"
-        onChange={(event) => {
-          handleChange(event);
-        }}
-      >
-        <label
-          for="zone"
-          className="block text-left text-sm font-medium text-gray-700 mb-2"
+      <div className=" flex justify-items-start space-x-18">
+        {/*  Zona dropdown --> */}
+        <div
+          className="mb-6"
+          onChange={(event) => {
+            handleChange(event);
+          }}
         >
-          Zona
-        </label>
-        <select
-          id="zone"
-          name="zone"
-          className="w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-500"
-        >
-          <option value="">Seleccionar zona</option>
-          <option value="SAN PEDRO">SAN PEDRO</option>
-          <option value="LA MANGA">LA MANGA</option>
-          <option value="MALAGA">MALAGA</option>
-          <option value="ALGECIRAS">ALGECIRAS</option>
-          <option value="FUENGIROLA">FUENGIROLA</option>
-          <option value="HUELVA">HUELVA</option>
-          <option value="CHICLANA">CHICLANA</option>
-        </select>
-      </div>
+          <label
+            for="zone"
+            className="block text-left text-sm font-medium text-gray-700 mb-2"
+          >
+            Zona
+          </label>
+          <select
+            id="zone"
+            name="zone"
+            className="w-full px-4 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-500"
+          >
+            <option value="">Seleccionar zona</option>
+            {zones.map((zone) => {
+              return (
+                <option key={zone} value={zone}>
+                  {zone}
+                </option>
+              );
+            })}
+          </select>
+        </div>
 
-      {/* <!-- Clientes afectados --> */}
-      <div
-        className="mb-6"
-        onChange={(event) => {
-          handleChange(event);
-        }}
-      >
-        <label
-          for="clientes"
-          className="text-left block text-sm font-medium text-gray-700 mb-2"
-        >
-          Clientes afectados
-        </label>
-        <input
-          name="affected"
-          type="number"
-          id="clientes"
-          min="0"
-          placeholder="Número de clientes"
-          className="w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-500"
-        />
+        {/* Dirección */}
+        <div className="">
+          <label
+            for="direccion"
+            className="text-left block text-sm font-medium text-gray-700 mb-2"
+          >
+            Dirección
+          </label>
+          <input
+            name="adress"
+            type="text"
+            id="direccion"
+            placeholder="Dirección"
+            onChange={(event) => {
+              handleChange(event);
+            }}
+            className="w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-500"
+          />
+        </div>
       </div>
 
       {/* <!-- Puertos --> */}
-      <div className="mb-6 w-full">
-        <label className="text-left block text-sm font-medium text-gray-700 mb-2">
-          Puertos
-        </label>
-        <div className="flex space-x-2">
-          <input
-            name="f"
-            type="text"
-            id="puerto1"
-            placeholder="f"
-            onChange={(event) => {
-              handleChange(event);
-            }}
-            className="w-1/3 px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-500"
-          />
-          <span className="flex items-center text-gray-500">/</span>
-          <input
-            name="s"
-            type="text"
-            id="puerto2"
-            placeholder="s"
-            onChange={(event) => {
-              handleChange(event);
-            }}
-            className="w-1/3 px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-500"
-          />
-          <span className="flex items-center text-gray-500">/</span>
-          <input
-            name="p"
-            type="text"
-            id="puerto3"
-            placeholder="p"
-            onChange={(event) => {
-              handleChange(event);
-            }}
-            className="w-1/3 px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-500"
-          />
-        </div>
-      </div>
-
-      {/* <!-- Fecha y hora--> */}
-      <div className="mb-6 flex justify-between">
+      <div className="flex flex-row space-x-18 mb-6">
+        {/* <!-- Fecha y hora--> */}
         <div className="flex flex-col">
           <label
             for="fecha"
@@ -158,6 +136,7 @@ export default function Form({ formData, handleChange }) {
             className="px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-500"
           />
         </div>
+
         <div className="flex flex-col">
           <label className="text-left text-sm font-medium text-gray-700 mb-2">
             Hora
@@ -173,94 +152,118 @@ export default function Form({ formData, handleChange }) {
           />
         </div>
       </div>
-      {/* Coordenadas */}
-      <div className="mb-6">
-        <label className="text-left block text-sm font-medium text-gray-700 mb-2">
-          Coordenadas
-        </label>
-        <div className="flex space-x-3">
-          <input
-            name="x"
-            type="text"
-            id="coord1"
-            placeholder="x"
-            onChange={(event) => {
-              handleChange(event);
-            }}
-            className="w-1/3 flex-1 px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-500"
-          />
-          <span className="flex items-center text-gray-500">,</span>
-          <input
-            name="y"
-            type="text"
-            id="coord2"
-            placeholder="y"
-            onChange={(event) => {
-              handleChange(event);
-            }}
-            className="w-1/3 flex-1 px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-500"
-          />
+
+      <div className="mb-6 flex space-x-24">
+        <div className="flex flex-col">
+          <label className="text-left block text-sm font-medium text-gray-700 mb-2">
+            Puertos
+          </label>
+          <div className="flex w-44 space-x-2">
+            <input
+              name="f"
+              type="text"
+              id="puerto1"
+              placeholder="f"
+              onChange={(event) => {
+                handleChange(event);
+              }}
+              className="w-1/3 px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-500"
+            />
+            <span className="flex items-center text-gray-500">/</span>
+            <input
+              name="s"
+              type="text"
+              id="puerto2"
+              placeholder="s"
+              onChange={(event) => {
+                handleChange(event);
+              }}
+              className="w-1/3 px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-500"
+            />
+            <span className="flex items-center text-gray-500">/</span>
+            <input
+              name="p"
+              type="text"
+              id="puerto3"
+              placeholder="p"
+              onChange={(event) => {
+                handleChange(event);
+              }}
+              className="w-1/3 px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-500"
+            />
+          </div>
+        </div>
+        {/* Coordenadas */}
+
+        <div className="flex flex-col">
+          <label className="text-left block text-sm font-medium text-gray-700 mb-2">
+            Coordenadas
+          </label>
+          <div className="flex w-24 space-x-2">
+            <input
+              name="x"
+              type="text"
+              id="coord1"
+              placeholder="x"
+              onChange={(event) => {
+                handleChange(event);
+              }}
+              className="w-1/3 flex-1 px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-500"
+            />
+            <span className="flex items-center text-gray-500">,</span>
+            <input
+              name="y"
+              type="text"
+              id="coord2"
+              placeholder="y"
+              onChange={(event) => {
+                handleChange(event);
+              }}
+              className="w-1/3 flex-1 px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-500"
+            />
+          </div>
         </div>
       </div>
 
-      {/* Dirección */}
-      <div className="mb-6">
-        <label
-          for="direccion"
-          className="text-left block text-sm font-medium text-gray-700 mb-2"
-        >
-          Dirección
-        </label>
-        <input
-          name="adress"
-          type="text"
-          id="direccion"
-          placeholder="Dirección"
-          onChange={(event) => {
-            handleChange(event);
-          }}
-          className="w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-500"
-        />
-      </div>
-
       {/* <!-- S/N --> */}
-      <div className="mb-6">
-        <label
-          for="sn"
-          className="text-left  block text-sm font-medium text-gray-700 mb-2"
-        >
-          SN
-        </label>
-        <input
-          name="serialNumber"
-          type="text"
-          id="sn"
-          placeholder="Número de serie"
-          onChange={(event) => {
-            handleChange(event);
-          }}
-          className="w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-500"
-        />
-      </div>
-
-      {/* <!-- Tarea --> */}
-      <div className="mb-8">
-        <label
-          for="tarea"
-          className="text-left block text-sm font-medium text-gray-700 mb-2"
-        >
-          Tarea
-        </label>
-        <textarea
-          name="ticketUrl"
-          id="tarea"
-          rows="1"
-          placeholder="Enlace tarea"
-          onChange={(event) => {
-            handleChange(event);
-          }}
-          className="w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-500 resize-none"
-        ></textarea>
+      <div className="mb-6 flex space-x-12">
+        <div className="flex flex-col">
+          <label
+            for="sn"
+            className="text-left  block text-sm font-medium text-gray-700 mb-2"
+          >
+            SN
+          </label>
+          <input
+            name="serialNumber"
+            type="text"
+            id="sn"
+            placeholder="Número de serie"
+            onChange={(event) => {
+              handleChange(event);
+            }}
+            className="w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-500"
+          />
+        </div>
+        {/* <!-- Tarea --> */}
+        <div className="">
+          <label
+            for="tarea"
+            className="text-left block text-sm font-medium text-gray-700 mb-2"
+          >
+            Tarea
+          </label>
+          <textarea
+            name="ticketUrl"
+            id="tarea"
+            rows="1"
+            placeholder="Enlace tarea"
+            onChange={(event) => {
+              handleChange(event);
+            }}
+            className="px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-500 resize-none"
+          ></textarea>
+        </div>
       </div>
     </div>
   );
--- incident-formatter/src/Output.jsx
+++ incident-formatter/src/Output.jsx
@@ -1,14 +1,22 @@
 import { useState } from "react";
-import { DocumentDuplicateIcon } from "@heroicons/react/24/solid";
 
 export default function Output({ formData }) {
   const [copied, setCopied] = useState(false);
 
   function copyNumber() {
-    //navigator.clipboard.writeText(formData);
+    const formDataToCopy = `LOS ${formData.type} ROJO - ZONA ${formData.zone} ${formData.affected} CLIENTES AFECTADOS
+Puertos: ${formData.gpon.f} / ${formData.gpon.s} / ${formData.gpon.p}
+Fecha y hora: ${formData.date}, ${formData.time}
+Coordenadas: ${formData.coordinates.x}, ${formData.coordinates.y}
+Dirección: ${formData.adress}
+SN: ${formData.serialNumber}
+Tarea: ${formData.ticketUrl}`;
+
+    navigator.clipboard.writeText(formDataToCopy);
 
     setCopied(true);
   }
+
   return (
     <div className="">
       <div className="border rounded-xs p-10 ">
@@ -29,7 +37,12 @@ export default function Output({ formData }) {
           </p>
           <p>Dirección: {formData.adress}</p>
           <p>SN: {formData.serialNumber}</p>
-          <p>Tarea: {formData.ticketUrl}</p>
+          <p>
+            Tarea:{" "}
+            <a href={formData.ticketUrl} target="_blank">
+              {formData.ticketUrl}
+            </a>
+          </p>
         </div>
       </div>
       <button
